0000b6700d1f55cb7005ae9cfa67135c4f4246d0
siddami <dharmmy621@gmail.com>
2024-01-02 23:00:46 +0100

    added about me description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ function About(){
                     <div>
                         <h2>About Me</h2>
                         <h3>I am a Doctor of Philosophy and a Lecturer with over 10 years of experience</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eligendi nemo neque quaerat ad voluptates modi, non odio. Praesentium dignissimos tenetur sed perspiciatis a adipisci quaerat vel illum totam sunt?</p>
+                        <p>With over a decade of expertise as a Doctor of Philosophy specializing in education and management, I have dedicated my career to academia. As a seasoned lecturer, associate professor, and committed researcher, my passion lies in advancing knowledge and fostering critical thinking within the realms of education and management.</p>
                         {/* stats */}
                         <div>
                             <div>
