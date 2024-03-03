f65fc15c2a0bf3d36821b730f3c1bc941f7a50e4
Siddami <dharmmy621@gmail.com>
2024-03-03 18:54:50 +0100

    uncommencted other elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
-// import Services from "./components/Services"
-// import Work from "./components/Work"
-// import Contact from "./components/Contact"
+import Services from "./components/Services";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,9 +13,10 @@ function App() {
       <Banner />
       <NavBar />
       <About />
-      {/* <Services />
+      <Services />
+      /
       <Work />
-      <Contact /> */}
+      <Contact />
       <div className="h-[4000px]"></div>
     </div>
   );
