e17700a9e6c8520100cf1a479a55e281df8d5e2b
Siddami <dharmmy621@gmail.com>
2024-03-07 20:09:33 +0100

    added images to work file

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,21 @@
-function Work(){
-    return(
-        <div className="section" id="work">
-            Work
-        </div>
-    )
-}
-export default Work
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
+//img
+import img1 from "./images/portfolio-img1.jpg";
+import img2 from "./images/portfolio-img2.png";
+import img3 from "./images/portfolio-img3.jpg";
+
+const Work =
+  () => {
+    return (
+      <section
+        className="section"
+        id="work"
+      >
+        Work
+      </section>
+    );
+  };
+export default Work;
